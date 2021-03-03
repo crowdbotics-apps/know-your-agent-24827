3dddd99eecfe522c0c169e41534285e3716c53af
Crowdbotics <team@crowdbotics.com>
2021-03-03 18:24:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/know_your_agent_24827/MainActivity.java
@@ -0,0 +1,15 @@
+package com.know_your_agent_24827;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "know_your_agent_24827";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://know-your-agent-24827.botics.co",
+  defaultTimeout: 5000
+};
